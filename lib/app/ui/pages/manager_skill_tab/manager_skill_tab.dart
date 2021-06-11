f82488916068a5e4c11f2fd6115cf9055059afbd
Dominic Needham <dominic.needham@bt.com>
2021-06-11 11:53:33 +0100

    simplify the skill list to reduce widget count

--- lib/app/ui/pages/manager_skill_tab/manager_skill_tab.dart
+++ lib/app/ui/pages/manager_skill_tab/manager_skill_tab.dart
@@ -5,7 +5,6 @@ import 'package:ssa_app/app/data/models/skill/category.dart';
 import 'package:ssa_app/app/data/models/skill/manager_staff_skill.dart';
 import 'package:ssa_app/app/ui/global_widgets/future_state_text.dart';
 import 'package:ssa_app/app/ui/global_widgets/loading_indicator.dart';
-import 'package:ssa_app/app/ui/global_widgets/page_title.dart';
 import 'package:ssa_app/app/ui/global_widgets/skill_list.dart';
 import 'package:ssa_app/app/ui/pages/manager_skill_tab/manager_skill_card.dart';
 
@@ -14,37 +13,30 @@ class ManagerSkillTab extends GetWidget<ManagerSkillTabController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Skills")),
-      body: ListView(
-        physics: ClampingScrollPhysics(),
-        children: [
-          PageTitle(text: "Skills"),
-          FutureBuilder(
-            future: controller.skills,
-            builder: (BuildContext ctx,
-                AsyncSnapshot<Map<Category, List<ManagerStaffSkill>>?>
-                    snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return LoadingIndicator();
-              }
-              if (snapshot.hasData) {
-                final skills = snapshot.data!;
-                if (skills.isEmpty) {
-                  return FutureStateText(text: "No skills loaded.");
-                }
-                return SkillList(
-                  gridChildAspectRatio: 2.5,
-                  skills: skills,
-                  cardBuilder: (skill) =>
-                      ManagerSkillCard(skill: skill as ManagerStaffSkill),
-                );
-              }
-              if (snapshot.hasError) {
-                return FutureStateText(text: "Oh no, that didn't work.");
-              }
-              return FutureStateText(text: "Looks like that didn't work.");
-            },
-          ),
-        ],
+      body: FutureBuilder(
+        future: controller.skills,
+        builder: (BuildContext ctx,
+            AsyncSnapshot<Map<Category, List<ManagerStaffSkill>>?> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LoadingIndicator();
+          }
+          if (snapshot.hasData) {
+            final skills = snapshot.data!;
+            if (skills.isEmpty) {
+              return FutureStateText(text: "No skills loaded.");
+            }
+            return SkillList(
+              gridChildAspectRatio: 2.5,
+              skills: skills,
+              cardBuilder: (skill) =>
+                  ManagerSkillCard(skill: skill as ManagerStaffSkill),
+            );
+          }
+          if (snapshot.hasError) {
+            return FutureStateText(text: "Oh no, that didn't work.");
+          }
+          return FutureStateText(text: "Looks like that didn't work.");
+        },
       ),
     );
   }
--- lib/app/ui/pages/staff_skill_tab/staff_skill_tab.dart
+++ lib/app/ui/pages/staff_skill_tab/staff_skill_tab.dart
@@ -6,7 +6,6 @@ import 'package:ssa_app/app/data/models/skill/category.dart';
 import 'package:ssa_app/app/data/models/skill/staff_skill.dart';
 import 'package:ssa_app/app/ui/global_widgets/future_state_text.dart';
 import 'package:ssa_app/app/ui/global_widgets/loading_indicator.dart';
-import 'package:ssa_app/app/ui/global_widgets/page_title.dart';
 import 'package:ssa_app/app/ui/global_widgets/skill_list.dart';
 import 'package:ssa_app/app/ui/pages/staff_skill_tab/staff_skill_card.dart';
 
@@ -15,38 +14,32 @@ class StaffSkillTab extends GetWidget<StaffSkillTabController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Skills'),
+        title: Text('Your Skills'),
       ),
-      body: ListView(
-        physics: ClampingScrollPhysics(),
-        children: [
-          PageTitle(text: "Your Skills"),
-          FutureBuilder(
-            future: controller.skills,
-            builder: (BuildContext ctx,
-                AsyncSnapshot<Map<Category, List<StaffSkill>>?> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return LoadingIndicator();
-              }
-              if (snapshot.hasData) {
-                final skills = snapshot.data!;
-                if (skills.isEmpty) {
-                  return FutureStateText(text: "You have no registered skills");
-                }
-                return SkillList(
-                  gridChildAspectRatio: 2,
-                  skills: skills,
-                  cardBuilder: (skill) =>
-                      StaffSkillCard(skill: skill as StaffSkill),
-                );
-              }
-              if (snapshot.hasError) {
-                return FutureStateText(text: "Oh no, that didn't work.");
-              }
+      body: FutureBuilder(
+        future: controller.skills,
+        builder: (BuildContext ctx,
+            AsyncSnapshot<Map<Category, List<StaffSkill>>?> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LoadingIndicator();
+          }
+          if (snapshot.hasData) {
+            final skills = snapshot.data!;
+            if (skills.isEmpty) {
               return FutureStateText(text: "You have no registered skills");
-            },
-          ),
-        ],
+            }
+            return SkillList(
+              gridChildAspectRatio: 2,
+              skills: skills,
+              cardBuilder: (skill) =>
+                  StaffSkillCard(skill: skill as StaffSkill),
+            );
+          }
+          if (snapshot.hasError) {
+            return FutureStateText(text: "Oh no, that didn't work.");
+          }
+          return FutureStateText(text: "You have no registered skills");
+        },
       ),
     );
   }
