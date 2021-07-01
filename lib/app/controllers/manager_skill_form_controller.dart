240f9d37f53cb1509642141e8cb7dc68f7ea57aa
Dominic Needham <dominic.needham@bt.com>
2021-07-01 20:40:00 +0100

    support for creating a new skill

--- lib/app/controllers/manager_skill_form_controller.dart
+++ lib/app/controllers/manager_skill_form_controller.dart
@@ -1,7 +1,68 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ssa_app/app/data/models/skill/category.dart';
+import 'package:ssa_app/app/data/repository/category_repository.dart';
 
 class ManagerSkillFormController extends GetxController {
-  final _obj = ''.obs;
-  set obj(value) => this._obj.value = value;
-  get obj => this._obj.value;
+  final categoryRepository = Get.find<CategoryRepository>();
+
+  final isLoading = true.obs;
+  final isError = false.obs;
+  final error = "".obs;
+
+  final _formKey = GlobalKey<FormState>();
+  GlobalKey<FormState> get formKey => _formKey;
+
+  final _categories = RxList<Category>();
+  RxList<Category> get categories => _categories;
+  final _selectedCategoryId = 0.obs;
+  int get selectedCategoryId => _selectedCategoryId.value;
+  set selectedCategoryId(int? value) {
+    if (value != null) _selectedCategoryId.value = value;
+  }
+
+  @override
+  void onInit() async {
+    super.onInit();
+    await fetchCategories();
+  }
+
+  Future<void> fetchCategories() async {
+    try {
+      isLoading.value = true;
+      final cats = await categoryRepository.categories;
+      if (cats.isNotEmpty) {
+        selectedCategoryId = cats[0].id;
+      }
+      _categories.addAll(cats);
+    } catch (e) {
+      isError.value = true;
+      error.value = e.toString();
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  String? validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a name';
+    }
+    return null;
+  }
+
+  String? validateCategory(int? value) {
+    if (value == null) {
+      return 'Please enter some text';
+    }
+    return null;
+  }
+
+  void save() {
+    final status = _formKey.currentState?.validate();
+    if (status ?? false) {
+      Get.snackbar("Excellent", "Forms looking good");
+    } else {
+      Get.snackbar("Terrible", "Forms looking not so good");
+    }
+  }
 }
--- lib/app/ui/pages/manager_skill_form_page/manager_skill_form_page.dart
+++ lib/app/ui/pages/manager_skill_form_page/manager_skill_form_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ssa_app/app/controllers/manager_skill_form_controller.dart';
+import 'package:ssa_app/app/ui/global_widgets/future_state_text.dart';
+import 'package:ssa_app/app/ui/global_widgets/loading_indicator.dart';
+import 'package:ssa_app/app/ui/pages/manager_skill_form_page/manager_skill_form.dart';
 
-class ManagerSkillFormPage extends StatelessWidget {
+class ManagerSkillFormPage extends GetWidget<ManagerSkillFormController> {
   const ManagerSkillFormPage({Key? key}) : super(key: key);
 
   @override
@@ -8,10 +13,28 @@ class ManagerSkillFormPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Add Skill"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: controller.save,
+          )
+        ],
       ),
-      body: Center(
-        child: Text("Add/Edit Skill"),
-      ),
+      body: Obx(() {
+        if (controller.isError.value) {
+          return FutureStateText(text: controller.error.value);
+        }
+        if (controller.isLoading.value) {
+          return LoadingIndicator();
+        }
+        if (controller.categories.isNotEmpty) {
+          return ManagerSkillForm();
+        }
+        if (controller.categories.isEmpty) {
+          return FutureStateText(text: "You must create a category first");
+        }
+        return FutureStateText(text: "Unknown state");
+      }),
     );
   }
 }
