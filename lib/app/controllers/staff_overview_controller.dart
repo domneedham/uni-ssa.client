f62f12e73e4b3b883632d30847bf7e8cd5c21443
Dominic Needham <dominic.needham@bt.com>
2021-07-10 10:28:19 +0100

    made staff overview page to design

--- lib/app/controllers/staff_overview_controller.dart
+++ lib/app/controllers/staff_overview_controller.dart
@@ -1,3 +1,88 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:ssa_app/app/data/models/skill/staff_skill.dart';
+import 'package:ssa_app/app/data/models/user/manager.dart';
+import 'package:ssa_app/app/data/models/user/staff.dart';
+import 'package:ssa_app/app/data/repository/skill_staff_repository.dart';
+import 'package:ssa_app/app/data/repository/user_repository.dart';
 
-class StaffOverviewController extends GetxController {}
+class StaffOverviewController extends GetxController {
+  final userRepo = Get.find<UserRepository>();
+  final skillRepo = Get.find<SkillStaffRepository>();
+
+  final parameters = Get.parameters;
+  final arguments = Get.arguments;
+
+  final isLoading = true.obs;
+  final isError = false.obs;
+  final error = "".obs;
+
+  Rx<Staff>? staff;
+
+  @override
+  void onInit() async {
+    super.onInit();
+    await getUser(parameters["id"]!);
+  }
+
+  Future<void> getUser(String id) async {
+    try {
+      isLoading.value = true;
+      int parsedId = int.parse(id);
+      Staff fetchedStaff = await userRepo.getStaffById(parsedId);
+      staff = fetchedStaff.obs;
+    } catch (e) {
+      isError.value = true;
+      error.value = e.toString();
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<Manager>? getManagerById(int id) async {
+    Manager fetchedManager = await userRepo.getManagerById(id);
+    return fetchedManager;
+  }
+
+  Future<StaffSkill>? getSkillById(int id) async {
+    StaffSkill fetchedSkill = await skillRepo.getSkillByIdAsync(id);
+    return fetchedSkill;
+  }
+
+  Future<List<StaffSkill>> getSkillsByIds(List<int> ids) async {
+    List<StaffSkill> fetchedSkills = await skillRepo.getSkillsByIds(ids);
+    return fetchedSkills;
+  }
+
+  String expiryText(StaffSkill skill) {
+    if (skill.hasExpiry) {
+      if (skill.isExpired) {
+        return "Expired";
+      } else {
+        return "Not Expired";
+      }
+    } else {
+      return "No Expiry";
+    }
+  }
+
+  Text expiryTextWidget(StaffSkill skill) {
+    if (skill.hasExpiry) {
+      final date = DateFormat.yMMMd().format(skill.expires!);
+      if (skill.isExpired) {
+        return Text(
+          "Expired on: $date",
+          style: TextStyle(color: Colors.red),
+        );
+      } else {
+        return Text(
+          "Expires on: $date",
+          style: TextStyle(color: Colors.green),
+        );
+      }
+    } else {
+      return Text("No Expiry");
+    }
+  }
+}
--- lib/app/ui/pages/user_overview/staff_overview/staff_overview_page.dart
+++ lib/app/ui/pages/user_overview/staff_overview/staff_overview_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ssa_app/app/controllers/staff_overview_controller.dart';
+import 'package:ssa_app/app/data/models/skill/staff_skill.dart';
+import 'package:ssa_app/app/data/models/user/manager.dart';
+import 'package:ssa_app/app/ui/global_widgets/future_state_text.dart';
+import 'package:ssa_app/app/ui/global_widgets/loading_indicator.dart';
+import 'package:ssa_app/app/ui/global_widgets/user_profile_header.dart';
 
-class StaffOverviewPage extends StatelessWidget {
+class StaffOverviewPage extends GetWidget<StaffOverviewController> {
   const StaffOverviewPage({Key? key}) : super(key: key);
 
   @override
@@ -9,11 +16,82 @@ class StaffOverviewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Staff Overview"),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Staff overview"),
-        ),
-      ),
+      body: Obx(() {
+        if (controller.staff != null) {
+          final staff = controller.staff!.value;
+          return ListView(
+            children: [
+              UserProfileHeader(user: staff),
+              const Divider(),
+              const Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text("Reports To:"),
+              ),
+              FutureBuilder(
+                future: controller.getManagerById(staff.managerId),
+                builder: (BuildContext ctx, AsyncSnapshot<Manager> snapshot) {
+                  if (snapshot.hasData) {
+                    final user = snapshot.data!;
+                    return ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text(user.name),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return const ListTile(
+                      leading: Icon(Icons.person_off),
+                      title: Text("User failed to load"),
+                    );
+                  }
+                  return const ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Loading user..."),
+                  );
+                },
+              ),
+              const Divider(),
+              const Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text("Skill List:"),
+              ),
+              FutureBuilder(
+                future: controller.getSkillsByIds(staff.skills),
+                builder: (BuildContext ctx,
+                    AsyncSnapshot<List<StaffSkill>> snapshot) {
+                  if (snapshot.hasData) {
+                    final skills = snapshot.data!;
+                    return ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: skills.length,
+                      itemBuilder: (ctx, pos) {
+                        final skill = skills[pos];
+                        return ListTile(
+                          leading: Icon(skill.category.icon),
+                          title: Text(skill.name),
+                          subtitle: controller.expiryTextWidget(skill),
+                        );
+                      },
+                    );
+                  }
+
+                  if (snapshot.hasError) {
+                    return FutureStateText(text: "Failed to load skills");
+                  }
+                  return LoadingIndicator();
+                },
+              ),
+            ],
+          );
+        }
+        if (controller.isError.value) {
+          return FutureStateText(text: controller.error.value);
+        }
+        if (controller.isLoading.value) {
+          return const LoadingIndicator();
+        }
+        return const FutureStateText(text: "Unknown state");
+      }),
     );
   }
 }
